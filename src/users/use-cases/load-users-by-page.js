bae8792647b7da8e5eb5ece92d37b2c5c15777a0
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-14 13:02:53 -0500

    App uploaded pending debugging

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -10,9 +10,19 @@ export const loadUsersByPage = async( page = 1 ) => {
 
     const url = `${ import.meta.env.VITE_BASE_URL }/users?_page=${ page }`;
     const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Error fetching data: ${res.status}`);
+    }
+
     const data = await res.json();
 
-    const users = data.map( localhostUserToModel );
+    // Verificar si data es un array antes de usar map
+    if (!Array.isArray(data)) {
+        throw new Error('Data is not an array');
+    }
+
+    const users = data.map(localhostUserToModel);
 
     return users;
 }
